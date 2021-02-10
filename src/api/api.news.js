339d8b63a725bab5bef6e82b38f594844d90e72f
DC <jessy.charlot@outlook.fr>
2021-02-10 19:57:23 +0100

    Improve api calls

--- src/api/api.news.js
+++ src/api/api.news.js
@@ -1,6 +1,15 @@
 async function getTopStoriesID() {
     const TOP_STORIES_ENDPOINT = 'https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty'
-    const responseData = fetch(TOP_STORIES_ENDPOINT).then(res => res.json())
+    const options = {
+        method : 'GET',
+        headers : {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
+
+    const responseData = fetch(TOP_STORIES_ENDPOINT, options).then(res => res.json())
     return responseData;
 }
 
@@ -9,8 +18,16 @@ async function getStoryFromID(storyId = 0) {
         throw new Error("ID is required for fetch story")
     }
 
+    const options = {
+        method : 'GET',
+        headers : {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }
+
     const STORY_DATA_ENDPOINT = `https://hacker-news.firebaseio.com/v0/item/${storyId}.json?print=pretty`
-    const responseData = fetch(STORY_DATA_ENDPOINT).then(res => res.json())
+    const responseData = fetch(STORY_DATA_ENDPOINT, options).then(res => res.json())
     return responseData;
 }
 
