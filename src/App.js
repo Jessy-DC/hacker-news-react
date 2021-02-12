d396b23f0ddad71da72e3ed97b003277cd43d562
DC <jessy.charlot@outlook.fr>
2021-02-12 19:52:20 +0100

    Redesign web site

--- src/App.js
+++ src/App.js
@@ -4,20 +4,32 @@ import {Header} from "./components/Header";
 import {Router, Route, Switch} from "react-router-dom";
 import {createBrowserHistory} from 'history'
 import {Welcome} from "./components/Welcome";
+import {ThemeProvider} from "styled-components";
+import {GlobalStyles} from "./components/GlobalStyle";
 
 const history = createBrowserHistory();
 
+const darkMode = {
+    body: '#1a1d29',
+    text: '#FAFAFA',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
+
 function App() {
   return (
-      <Router history={history}>
-        <div className="App">
-            <Header />
-            <Switch>
-                <Route exact path='/' component={NewsList} />
-                <Route path='/welcome' component={Welcome} />
-            </Switch>
-        </div>
-      </Router>
+      <ThemeProvider theme={darkMode}>
+          <GlobalStyles />
+          <Router history={history}>
+              <div className="App">
+                  <Header />
+                  <Switch>
+                      <Route exact path='/' component={NewsList} />
+                      <Route path='/welcome' component={Welcome} />
+                  </Switch>
+              </div>
+          </Router>
+      </ThemeProvider>
   );
 }
 
